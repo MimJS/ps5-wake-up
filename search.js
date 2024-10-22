c6053282bee537be9e255220f8e904ca5efc6cf1
MimJS <88341555+MimJS@users.noreply.github.com>
2024-10-22 17:27:24 +0300

    update search

--- search.js
+++ search.js
@@ -3,7 +3,6 @@ const client = dgram.createSocket("udp4");
 
 const _DDP_VERSION = "00030010";
 
-// Using template literals to construct the formatted string
 const pkt_output = `
 SRCH * HTTP/1.1
 device-discovery-protocol-version:${_DDP_VERSION}
@@ -11,16 +10,7 @@ device-discovery-protocol-version:${_DDP_VERSION}
 
 const message = Buffer.from(pkt_output);
 const SERVER_PORT = 9302;
-const SERVER_HOST = "192.168.1.105";
-
-client.send(message, SERVER_PORT, SERVER_HOST, (error) => {
-  if (error) {
-    console.log("Error sending message:", error);
-    client.close();
-  } else {
-    console.log("Message sent!");
-  }
-});
+const BROADCAST_IP = "192.168.1.255";
 
 client.on("message", (responseMessage, remote) => {
   console.log(
@@ -29,4 +19,17 @@ client.on("message", (responseMessage, remote) => {
     } - ${responseMessage.toString()}`
   );
   client.close();
+});
+
+client.bind(() => {
+  client.setBroadcast(true);
+
+  client.send(message, SERVER_PORT, BROADCAST_IP, (error) => {
+    if (error) {
+      console.log("Error sending message:", error);
+      client.close();
+    } else {
+      console.log("Message sent to broadcast address!");
+    }
+  });
 });
