9fc165c0a17e363669d00a3bbb90b9cb8fe3f773
MimJS <88341555+MimJS@users.noreply.github.com>
2024-10-22 19:40:37 +0300

    fix

--- standby.js
+++ standby.js
@@ -2,20 +2,11 @@ const axios = require("axios");
 
 const config = {
   method: "get",
-  url: "http://192.168.1.105:9295/sie/ps5/rp/sess/ctrl",
+  url: "http://192.168.1.105:9295/sie/ps5/rp/sess/init",
   headers: {
     "User-Agent": "remoteplay Windows",
-    "rp-auth": "3Z4dEgzQwCxjsnuNWxmEAg==",
-    "rp-version": "1.0",
-    "rp-did": "8nOixzdlwf/th4MZnFHjO1diZVYLW6QrVX5lkv4y//8=",
-    "rp-controllertype": "3",
-    "rp-clienttype": "11",
-    "rp-ostype": "MNOCnlbrud2k",
-    "rp-conpath": "1",
-    "rp-startbitrate": "6Wi/7A==",
-    "rp-streamingtype": "BIzprQ==",
-    "Content-Length": "0",
-    Connection: "keep-alive",
+    "rp-registkey": 3862316466663564,
+    "rp-version": 1.0,
   },
 };
 
